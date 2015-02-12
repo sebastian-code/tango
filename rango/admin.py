80424bb299e6f8b156e3199f1257b814cc09c2d4
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-02-12 11:03:54 -0500

    Se modifica la vista y la plantilla para empezar a cumplir con los requerimientos establecidos en el ejercicio.

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'url_display')
+
+	def url_display(self, obj):
+		return u"<a href='%s'>%s</a>" % (obj.url, obj.url)
+
+	url_display.allow_tags = True
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
